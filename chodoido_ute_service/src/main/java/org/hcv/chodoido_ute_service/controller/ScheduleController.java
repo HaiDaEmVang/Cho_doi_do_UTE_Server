6c0003047712224d6439c118210e9024c641a676
LeHai <lengochai7304@gmail.com>
2024-12-06 14:26:05 +0700

    add schedule

--- chodoido_ute_service/src/main/java/org/hcv/chodoido_ute_service/controller/ScheduleController.java
+++ chodoido_ute_service/src/main/java/org/hcv/chodoido_ute_service/controller/ScheduleController.java
@@ -1,15 +1,16 @@
 package org.hcv.chodoido_ute_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/schedule")
+@Slf4j
 public class ScheduleController {
-    @Scheduled(fixedRate = 300000)
-    @GetMapping("/heath")
+    @Scheduled(fixedRate = 1000*60)
     public void schedule(){
+        log.info("server_healthy", "Server is live");
     }
 }
